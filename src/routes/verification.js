9a868fafd33fa0c1194aed09f558e157ebc5abd7
VishSeran <vishwa.sovis@gmail.com>
2025-09-21 13:48:48 +0530

    send verification updated

--- src/routes/verification.js
+++ src/routes/verification.js
@@ -2,12 +2,38 @@ import express from'express';
 import { Router } from 'express';
 import nodemailer from 'nodemailer';
 import {generateOTP} from '../utils/generateOTP.js';
+import notificationSchema from '../models/notification.js';
 
 const router = express.Router();
 
 const transporter = nodemailer.createTransport({
     service:'gmail',
     auth: {
-        user: process.env.
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+
     }
-})
+});
+
+//send verification
+router.post("/sendVerification", async (req,res) =>{
+    try{
+        const {userId,email} = req.body;
+        //otp generate
+        const otp = generateOTP(6);
+
+        //send OTP through email
+        await transporter.sendMail({
+            from: `"DeedLink Notification" <${process.env.EMAIL_USER}>`,
+            to:email,
+            subject:"Your verfication code",
+            text: `Your verification code is: ${otp}`,
+            html: `<p>Your verification code is: <b>${otp}</b></p><p>This code is valid for 10 minutes.</p>`
+        });
+
+        res.status(200).json({message: "Verification code sent to email"});
+    }catch(error){
+        console.error(error);
+        res.status(500).json({error:"Failed to send verification email"});
+    }
+});
