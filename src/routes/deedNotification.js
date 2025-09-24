0220901aa20ee06c86f30a1e59cfc0086467264e
VishSeran <vishwa.sovis@gmail.com>
2025-09-24 10:17:14 +0530

    deed registration updated

--- src/routes/deedNotification.js
+++ src/routes/deedNotification.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { Router } from 'express'
-import nodemailer from 'nodemailer'
+import nodemailer from 'nodemailer';
 
 const router = express.Router();
 
@@ -14,9 +14,18 @@ const transporter = nodemailer.createTestAccount({
 
 //verification email for deed registration
 router.post('/deedRegistration', async (req, res) => {
-    try{
-        const {userId,email} = req.body;
-    }catch(error){
+    try {
+        const { userId, email } = req.body;
 
+        await transporter.sendMail({
+            from: `"DeedLink Notification" <${process.env.EMAIL_USER}>`,
+            to: email,
+            subject: "Deed Registration Verified"
+        })
+
+        res.status(200).json({ message: "Verification code sent to email" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Failed to send verification email" });
     }
 })
