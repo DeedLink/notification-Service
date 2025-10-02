32cc10f1b4671e5ca6ecdfabf0aa21fe358a7f2a
VishSeran <vishwa.sovis@gmail.com>
2025-10-02 14:21:54 +0530

    Add emailService

---
+++ src/services/emailService.js
@@ -0,0 +1,22 @@
+import transporter from "../config/emailConfig";
+
+async function sendEmail(to, subject,message,html){
+    try{
+        const mailOption = {
+            from: process.env.EMAIL_USER,
+            to,
+            subject,
+            message,
+            html
+        }
+
+        let info = await transporter.sendMail(mailOption);
+        console.log("Email sent!", info.messageId);
+        return info;
+    }catch(e){
+        console.error("Error sending email",e);
+        throw e;
+    }
+}
+
+export default sendEmail;
